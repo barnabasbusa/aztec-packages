05ce1f77cefb2a687367f8a009c5d40917071fbf
Leila Wang <leizciw@gmail.com>
2025-08-21 13:54:50 +0000

    Refactor and tests.

--- yarn-project/bb-prover/src/bb/execute.ts
+++ yarn-project/bb-prover/src/bb/execute.ts
@@ -229,7 +229,7 @@ export async function generateProof(
     return { status: BB_RESULT.FAILURE, reason: `Working directory ${workingDirectory} does not exist` };
   }
 
-  // The bytecode is written to e.g. /workingDirectory/BaseParityArtifact-bytecode
+  // The bytecode is written to e.g. /workingDirectory/ParityBaseArtifact-bytecode
   const bytecodePath = `${workingDirectory}/${circuitName}-bytecode`;
 
   // The proof is written to e.g. /workingDirectory/ultra_honk/proof
@@ -688,7 +688,7 @@ export async function computeGateCountForCircuit(
     return { status: BB_RESULT.FAILURE, reason: `Working directory ${workingDirectory} does not exist` };
   }
 
-  // The bytecode is written to e.g. /workingDirectory/BaseParityArtifact-bytecode
+  // The bytecode is written to e.g. /workingDirectory/ParityBaseArtifact-bytecode
   const bytecodePath = `${workingDirectory}/${circuitName}-bytecode`;
 
   const binaryPresent = await fs
